adfa358c530612e58ccc25183b682046cdedd7cf
JonathanThomas <jonathanthomasbluebird@gmail.com>
2022-09-27 23:39:00 +0530

    Initial commit with css and html done. jss button listeners not working, will work later

---
+++ script.js
@@ -0,0 +1,19 @@
+const btns=document.querySelectorAll('button');
+btns.forEach((btn)=>{
+    btn.addEventListener('click')
+    //alert(id);
+});
+
+/*function sensor(){
+    btn.forEach((button)=>{
+        button.addEventListener('click', button.getAttribute('id'))
+        alert(id);
+    });
+}
+    
+sensor();*/
+
+
+
+
+
