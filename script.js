73f44232fb3c418e37df2a455f6470662573fb4e
JonathanThomas <jonathanthomasbluebird@gmail.com>
2022-09-29 12:50:31 +0530

    buttons and display working, calculation functions not done

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 const btns=document.querySelectorAll('button');
-btns.forEach((btn)=>{
-    btn.addEventListener('click')
-    //alert(id);
+const display=document.querySelector('#display');
+
+console.log(btns);
+btns.forEach(button=>{
+    button.addEventListener('click', ()=>{
+        let btnid=button.getAttribute('id');
+        let btnclass=button.getAttribute('class');
+        sensor(btnid, btnclass);
+
+    })
 });
 
-/*function sensor(){
-    btn.forEach((button)=>{
-        button.addEventListener('click', button.getAttribute('id'))
-        alert(id);
-    });
+function sensor(number, buttclass){
+    displayNumber=displayNumber+number;
+    display.textContent=displayNumber;
+    if(buttclass=='oper'){
+        console.log('op detected');
+        displayNumber=0;
+    }
+    console.log('working heyy')
 }
-    
-sensor();*/
 
+let displayNumber=0;
+display.textContent=displayNumber;
+    
+let calc={
+    n1:0,
+    op:'',
+    n2:0,
+    res:0
+}
 
 
 
